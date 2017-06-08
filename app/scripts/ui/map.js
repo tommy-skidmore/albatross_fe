baf6c13d3bd8ecbf6a6450a5b5107d0a2f779a55
Remek Zajac <remek.zajac@gmail.com>
2017-06-08 13:03:37 +0200

    add mapbox

--- app/scripts/ui/map.js
+++ app/scripts/ui/map.js
@@ -6,34 +6,48 @@
 
 'use strict';
 
-var when      = require("when");
+const when      = require("when");
 when.delay    = require("when/delay");
 
-var map = (function() {
+const map = (function() {
 
-    var mapElement = $("#map");
-    var mapWrap    = $("#mapWrap")
+    const mapElement = $("#map");
+    const initialCentre = new L.LatLng(49.977722, 14.064453);
 
-    var initialCentre = new L.LatLng(49.977722, 14.064453);
-    var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-    var map = L.map('map', {
-      center: initialCentre,
-      zoom: 5,
-      layers: [OpenStreetMap_Mapnik]
-    });
-    var baseMaps = {
-        "OpenCycleMap": L.tileLayer(
-            "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png", {
-            maxZoom: 18,
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
-        }),
-        "Google Aerial": new L.Google()
-    };
-    L.control.layers(baseMaps).addTo(map);
+    function leaflet() {
+        let OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        });
+        let map = L.map('map', {
+          center: initialCentre,
+          zoom: 5,
+          layers: [OpenStreetMap_Mapnik]
+        });
+        let baseMaps = {
+            "OpenCycleMap": L.tileLayer(
+                "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png", {
+                maxZoom: 18,
+                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+            }),
+            "Google Aerial": new L.Google()
+        };
+        L.control.layers(baseMaps).addTo(map);
+    }
 
+    function mapbox() {
+        mapboxgl.accessToken = 'pk.eyJ1IjoicmVtc3RlciIsImEiOiJjaXF6MnlrYXUwMDY3aTVubmxxdWN2M2htIn0.8FBrAn804OlX9QYW-FRVWA'
+        const map = new mapboxgl.Map({
+            container  : 'map',
+            zoom       : 10,
+            center     : initialCentre,
+            style      : 'mapbox://styles/mapbox/mapbox.mapbox-terrain-v2',
+            bearingSnap: 10,
+            dragRotate : false //https://github.com/mapbox/mapbox-gl-js/issues/4297
+        });
+    }
+
+    mapbox();
     function test() { return true; }
 
     return {
